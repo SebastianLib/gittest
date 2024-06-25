db71ffb33077b411fccc90edbde281e2071b5653
SebastianLib <sebastianlib04@gmail.com>
2024-06-25 22:19:30 +0200

    created contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Contact from "./contact";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         loool
         test
+        <Contact/>
     </main>
   );
 }
